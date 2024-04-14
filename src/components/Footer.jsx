da64b3be9d2ee7a3b410d1821f72ec3c176534fb
nilabh joshi <nilabhrrr@gmail.com>
2024-04-14 20:51:20 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">NJFOODS</div>
+          <div className="right">
+            <p>Street 27, Mumbai</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By NilabhJoshi</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By NJFOODS.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
